93314bae8143508fcc9828d4c709bc4712512a1d
makingsoundmachines <roland@groovelastig.de>
2021-03-30 22:22:42 +0200

    Board updates

---
+++ src/daisy_heart-of-gold.cpp
@@ -0,0 +1,360 @@
+
+#include "daisy_heart-of-gold.h"
+#include "dev/codec_ak4556.h"
+
+using namespace daisy;
+
+/** MSM modified for use with Heart of Gold */
+
+// Hardware Definitions
+#define PIN_ENC_1_CLICK 0   // HoG Enc 1 Click PB12
+#define PIN_ENC_1_A 11      // HoG Enc 1 Click PB8
+#define PIN_ENC_1_B 12      // HoG Enc 1 Click PB9
+
+#define PIN_ENC_2_CLICK 44  // PC15
+#define PIN_ENC_2_A 30      // PB15
+#define PIN_ENC_2_B 45      // PB13
+
+#define PIN_ENC_3_CLICK 46  // PD13
+#define PIN_ENC_3_A 47      // PD7
+#define PIN_ENC_3_B 48      // PD6
+
+#define PIN_ENC_4_CLICK 49  // PG12
+#define PIN_ENC_4_A 7       // PG10
+#define PIN_ENC_4_B 50      // PG7
+
+#define PIN_MIDI_OUT 13   // HoG MIDI Out PB6
+#define PIN_MIDI_IN 14    // HoG MIDI In PB7
+
+#define PIN_GATE_OUT_1 17 // HoG Trig Out 1 PA8
+#define PIN_GATE_OUT_2 39 // PC6
+#define PIN_GATE_OUT_3 40 // PD12
+#define PIN_GATE_OUT_4 41 // PH6
+
+#define PIN_GATE_IN_1 20  // HoG Trig In 1 PB2
+#define PIN_GATE_IN_2 19  // HoG Trig In 2 PB10
+#define PIN_GATE_IN_3 42  // PC13
+#define PIN_GATE_IN_4 43  // PC14
+
+#define PIN_ROUTE_BUTTON_1 54  // PG13
+#define PIN_ROUTE_BUTTON_2 55  // PH7
+#define PIN_ROUTE_BUTTON_3 56  // PI8
+#define PIN_ROUTE_BUTTON_4 57  // PI11
+
+#define PIN_SAI_SCK_A 28  // HoG SAI2_SCK_B PA2
+#define PIN_SAI2_FS_A 27  // HoG SAI2_FS_B PG9
+#define PIN_SAI2_SD_A 26  // HoG SAI2_SD_A PD 11
+#define PIN_SAI2_SD_B 25  // HoG SAI2_SD_B PA0
+#define PIN_SAI2_MCLK 24  // HoG SAI2_MCLK_B PA1
+
+#define PIN_AK4556_RESET 29 // HoG PB14
+
+#define PIN_CTRL_1 21 // PC4
+#define PIN_CTRL_2 31 // PC1
+#define PIN_CTRL_3 32 // PA6
+#define PIN_CTRL_4 18 // PA7
+#define PIN_CTRL_5 33 // PB1
+#define PIN_CTRL_6 16 // PA3
+#define PIN_CTRL_7 15 // PC0
+#define PIN_CTRL_8 34 // PB0
+#define PIN_CTRL_9 35 // PC2 should be 35
+#define PIN_CTRL_10 36 // PC3 should be 36
+#define PIN_CTRL_11 37 // PC5 should be 37
+#define PIN_CTRL_12 38 // PH4 should be 38
+
+#define PIN_SR_DATA 51 // PD3
+#define PIN_SR_CLOCK 52 // PD5
+#define PIN_SR_LATCH 53 // PD4
+
+#define PIN_DAC8568_SYNC 9 //PB4
+
+void DaisyHeartOfGold::Init(bool boost)
+{
+    // Configure Seed first
+    seed.Configure();
+    seed.Init(boost);
+    InitAudio();
+    
+    InitCvOutputs();
+    InitEncoder();
+    InitGates();
+    InitRouteButtons();
+    InitSR595();
+    
+    InitDAC8568();
+    InitMidi();
+    InitControls();
+    // Reset AK4556
+    dsy_gpio_write(&ak4556_reset_pin_, 0);
+    DelayMs(10);
+    dsy_gpio_write(&ak4556_reset_pin_, 1);
+}
+
+void DaisyHeartOfGold::DelayMs(size_t del)
+{
+    seed.DelayMs(del);
+}
+
+void DaisyHeartOfGold::StartAudio(AudioHandle::AudioCallback cb)
+{
+    seed.StartAudio(cb);
+}
+
+void DaisyHeartOfGold::ChangeAudioCallback(AudioHandle::AudioCallback cb)
+{
+    seed.ChangeAudioCallback(cb);
+}
+
+void DaisyHeartOfGold::StopAudio()
+{
+    seed.StopAudio();
+}
+
+void DaisyHeartOfGold::SetAudioSampleRate(SaiHandle::Config::SampleRate samplerate)
+{
+    seed.SetAudioSampleRate(samplerate);
+}
+
+float DaisyHeartOfGold::AudioSampleRate()
+{
+    return seed.AudioSampleRate();
+}
+
+void DaisyHeartOfGold::SetAudioBlockSize(size_t size)
+{
+    seed.SetAudioBlockSize(size);
+}
+
+size_t DaisyHeartOfGold::AudioBlockSize()
+{
+    return seed.AudioBlockSize();
+}
+
+float DaisyHeartOfGold::AudioCallbackRate()
+{
+    return seed.AudioCallbackRate();
+}
+
+void DaisyHeartOfGold::StartAdc()
+{
+    seed.adc.Start();
+}
+
+/** Stops Transfering data from the ADC */
+void DaisyHeartOfGold::StopAdc()
+{
+    seed.adc.Stop();
+}
+
+//ex DaisyHeartOfGold::UpdateAnalogControls()
+void DaisyHeartOfGold::ProcessAnalogControls()
+{
+    for(size_t i = 0; i < CTRL_LAST; i++)
+    {
+        controls[i].Process();
+    }
+}
+
+//ex  DaisyHeartOfGold::GetCtrlValue(Ctrl k)
+float DaisyHeartOfGold::GetKnobValue(Ctrl k)
+{
+    return (controls[k].Value());
+}
+
+//ex DaisyHeartOfGold::DebounceControls()
+void DaisyHeartOfGold::ProcessDigitalControls()
+{
+    encoder[ENC_1].Debounce();
+    encoder[ENC_2].Debounce();
+    encoder[ENC_3].Debounce();
+    encoder[ENC_4].Debounce();
+}
+
+
+// Private Function Implementations
+// set SAI2 stuff -- run this between seed configure and init
+void DaisyHeartOfGold::InitAudio()
+{
+    // Handle Seed Audio as-is and then
+    SaiHandle::Config sai_config[2];
+    // Internal Codec
+    sai_config[0].periph          = SaiHandle::Config::Peripheral::SAI_1;
+    sai_config[0].sr              = SaiHandle::Config::SampleRate::SAI_48KHZ;
+    sai_config[0].bit_depth       = SaiHandle::Config::BitDepth::SAI_24BIT;
+    sai_config[0].a_sync          = SaiHandle::Config::Sync::MASTER;
+    sai_config[0].b_sync          = SaiHandle::Config::Sync::SLAVE;
+    sai_config[0].a_dir           = SaiHandle::Config::Direction::TRANSMIT;
+    sai_config[0].b_dir           = SaiHandle::Config::Direction::RECEIVE;
+    sai_config[0].pin_config.fs   = {DSY_GPIOE, 4};
+    sai_config[0].pin_config.mclk = {DSY_GPIOE, 2};
+    sai_config[0].pin_config.sck  = {DSY_GPIOE, 5};
+    sai_config[0].pin_config.sa   = {DSY_GPIOE, 6};
+    sai_config[0].pin_config.sb   = {DSY_GPIOE, 3};
+
+    // External Codec
+    sai_config[1].periph          = SaiHandle::Config::Peripheral::SAI_2;
+    sai_config[1].sr              = SaiHandle::Config::SampleRate::SAI_48KHZ;
+    sai_config[1].bit_depth       = SaiHandle::Config::BitDepth::SAI_24BIT;
+    sai_config[1].a_sync          = SaiHandle::Config::Sync::SLAVE;
+    sai_config[1].b_sync          = SaiHandle::Config::Sync::MASTER;
+    sai_config[1].a_dir           = SaiHandle::Config::Direction::TRANSMIT;
+    sai_config[1].b_dir           = SaiHandle::Config::Direction::RECEIVE;
+    sai_config[1].pin_config.fs   = seed.GetPin(27);
+    sai_config[1].pin_config.mclk = seed.GetPin(24);
+    sai_config[1].pin_config.sck  = seed.GetPin(28);
+    sai_config[1].pin_config.sb   = seed.GetPin(25);
+    sai_config[1].pin_config.sa   = seed.GetPin(26);
+
+    SaiHandle sai_handle[2];
+    sai_handle[0].Init(sai_config[0]);
+    sai_handle[1].Init(sai_config[1]);
+
+    // Reset Pin for AK4556
+    // Built-in AK4556 was reset during Seed Init
+    dsy_gpio_pin codec_reset_pin = seed.GetPin(29);
+    Ak4556::Init(codec_reset_pin);
+
+    // Reinit Audio for _both_ codecs...
+    AudioHandle::Config cfg;
+    cfg.blocksize  = 48;
+    cfg.samplerate = SaiHandle::Config::SampleRate::SAI_48KHZ;
+    cfg.postgain   = 0.5f;
+    seed.audio_handle.Init(cfg, sai_handle[0], sai_handle[1]);
+}
+
+void DaisyHeartOfGold::InitControls()
+{
+    AdcChannelConfig cfg[CTRL_LAST];
+
+    // Init ADC channels with Pins
+    cfg[CTRL_1].InitSingle({DSY_GPIOC, 4});  //seed.GetPin(PIN_CTRL_1));
+    cfg[CTRL_2].InitSingle({DSY_GPIOC, 1});  //seed.GetPin(PIN_CTRL_2));
+    cfg[CTRL_3].InitSingle({DSY_GPIOA, 6});  //seed.GetPin(PIN_CTRL_3));
+    cfg[CTRL_4].InitSingle({DSY_GPIOA, 7});  //seed.GetPin(PIN_CTRL_4));
+    cfg[CTRL_5].InitSingle({DSY_GPIOB, 1});  //seed.GetPin(PIN_CTRL_5));
+    cfg[CTRL_6].InitSingle({DSY_GPIOA, 3});  //seed.GetPin(PIN_CTRL_6));
+    cfg[CTRL_7].InitSingle({DSY_GPIOC, 0});  //seed.GetPin(PIN_CTRL_7));
+    cfg[CTRL_8].InitSingle({DSY_GPIOB, 0});  //seed.GetPin(PIN_CTRL_8));
+    cfg[CTRL_9].InitSingle({DSY_GPIOC, 2});  //seed.GetPin(PIN_CTRL_9));
+    cfg[CTRL_10].InitSingle({DSY_GPIOC, 3}); //seed.GetPin(PIN_CTRL_10));
+    cfg[CTRL_11].InitSingle({DSY_GPIOC, 5}); //seed.GetPin(PIN_CTRL_11));
+    //cfg[CTRL_12].InitSingle({DSY_GPIOH, 4}); //seed.GetPin(PIN_CTRL_12));
+
+    // Initialize ADC
+    //seed.adc.Init(cfg, CTRL_LAST);
+    seed.adc.Init(cfg, 11);
+
+    // Initialize AnalogControls, with flip set to true
+    // for(size_t i = 0; i < CTRL_LAST; i++)
+    for(size_t i = 0; i < 11; i++)
+    {
+        controls[i].Init(seed.adc.GetPtr(i), AudioCallbackRate(), true);
+    }
+}
+
+void DaisyHeartOfGold::InitDAC8568()
+{
+    dsy_gpio_pin pincfg[Dac8568::NUM_PINS];
+    pincfg[Dac8568::SYNC] = {DSY_GPIOB, 4};  //seed.GetPin(PIN_DAC8568_SYNC);
+    dac_8568.Init(pincfg);
+}
+
+void DaisyHeartOfGold::InitMidi()
+{
+    midi.Init(MidiHandler::MidiInputMode::INPUT_MODE_UART1,
+              MidiHandler::MidiOutputMode::OUTPUT_MODE_UART1);
+}
+
+void DaisyHeartOfGold::InitCvOutputs()
+{
+    DacHandle::Config cfg;
+    cfg.bitdepth   = DacHandle::BitDepth::BITS_12;
+    cfg.buff_state = DacHandle::BufferState::ENABLED;
+    cfg.mode       = DacHandle::Mode::POLLING;
+    cfg.chn        = DacHandle::Channel::BOTH;
+    seed.dac.Init(cfg);
+    seed.dac.WriteValue(DacHandle::Channel::BOTH, 0);
+}
+
+void DaisyHeartOfGold::InitEncoder()
+{
+    encoder[ENC_1].Init({DSY_GPIOB, 8},   //seed.GetPin(PIN_ENC_1_A), HoG v2 {DSY_GPIOC, 7}
+                        {DSY_GPIOB, 9},   //seed.GetPin(PIN_ENC_1_B), HoG v2 {DSY_GPIOG, 14}
+                        {DSY_GPIOB, 12},  //seed.GetPin(PIN_ENC_1_CLICK),
+                        AudioCallbackRate());
+
+    encoder[ENC_2].Init({DSY_GPIOB, 13},  //seed.GetPin(PIN_ENC_2_A)
+                        {DSY_GPIOB, 15},  //seed.GetPin(PIN_ENC_2_B)
+                        {DSY_GPIOC, 15},  //seed.GetPin(PIN_ENC_2_CLICK),
+                        AudioCallbackRate()); 
+
+    encoder[ENC_3].Init({DSY_GPIOD, 6},   //seed.GetPin(PIN_ENC_3_A)
+                        {DSY_GPIOD, 7},   //seed.GetPin(PIN_ENC_3_B)
+                        {DSY_GPIOD, 13},  //seed.GetPin(PIN_ENC_3_CLICK),
+                        AudioCallbackRate()); 
+
+    encoder[ENC_4].Init({DSY_GPIOG, 10},  //seed.GetPin(PIN_ENC_4_A)
+                        {DSY_GPIOG, 7},   //seed.GetPin(PIN_ENC_4_B)
+                        {DSY_GPIOG, 12},  //seed.GetPin(PIN_ENC_4_CLICK),
+                        AudioCallbackRate()); 
+}
+
+void DaisyHeartOfGold::InitSR595()
+{
+    num_sr_chained = 4;
+
+    sr_pin_cfg[SR_LATCH] = {DSY_GPIOD, 4};  //seed.GetPin(PIN_SR_LATCH);
+    sr_pin_cfg[SR_CLOCK] = {DSY_GPIOD, 5};  //seed.GetPin(PIN_SR_CLOCK);
+    sr_pin_cfg[SR_DATA]  = {DSY_GPIOD, 3};  //seed.GetPin(PIN_SR_DATA);
+
+    sr_595.Init(sr_pin_cfg, num_sr_chained);
+}
+
+void DaisyHeartOfGold::InitRouteButtons()
+{
+    // Route Buttons
+    dsy_gpio_pin pin;
+    pin = {DSY_GPIOG, 13};  //seed.GetPin(PIN_ROUTE_BUTTON_1);
+    route_button[ROUTE_BUTTON_1].Init(&pin);
+    pin = {DSY_GPIOH, 7};   //seed.GetPin(PIN_ROUTE_BUTTON_2);
+    route_button[ROUTE_BUTTON_2].Init(&pin);
+    pin = {DSY_GPIOI, 8};   //seed.GetPin(PIN_ROUTE_BUTTON_3);
+    route_button[ROUTE_BUTTON_3].Init(&pin);
+    pin = {DSY_GPIOI, 11};  //seed.GetPin(PIN_ROUTE_BUTTON_4);
+    route_button[ROUTE_BUTTON_4].Init(&pin);    
+}
+
+void DaisyHeartOfGold::InitGates()
+{
+    // Gate Output
+    gate_output[GATE_OUT_1].pin  = {DSY_GPIOA, 8};  //seed.GetPin(PIN_GATE_OUT_1);
+    gate_output[GATE_OUT_1].mode = DSY_GPIO_MODE_OUTPUT_PP;
+    gate_output[GATE_OUT_1].pull = DSY_GPIO_NOPULL;
+    dsy_gpio_init(&gate_output[GATE_OUT_1]);
+
+    gate_output[GATE_OUT_2].pin  = {DSY_GPIOC, 6};  //seed.GetPin(PIN_GATE_OUT_2);
+    gate_output[GATE_OUT_2].mode = DSY_GPIO_MODE_OUTPUT_PP;
+    gate_output[GATE_OUT_2].pull = DSY_GPIO_NOPULL;
+    dsy_gpio_init(&gate_output[GATE_OUT_2]);
+
+    gate_output[GATE_OUT_3].pin  = {DSY_GPIOD, 12}; //seed.GetPin(PIN_GATE_OUT_3);
+    gate_output[GATE_OUT_3].mode = DSY_GPIO_MODE_OUTPUT_PP;
+    gate_output[GATE_OUT_3].pull = DSY_GPIO_NOPULL;
+    dsy_gpio_init(&gate_output[GATE_OUT_3]);
+
+    gate_output[GATE_OUT_4].pin  = {DSY_GPIOH, 6};  //seed.GetPin(PIN_GATE_OUT_4);
+    gate_output[GATE_OUT_4].mode = DSY_GPIO_MODE_OUTPUT_PP;
+    gate_output[GATE_OUT_4].pull = DSY_GPIO_NOPULL;
+    dsy_gpio_init(&gate_output[GATE_OUT_4]);            
+
+    // Gate Inputs
+    dsy_gpio_pin pin;
+    pin = {DSY_GPIOB, 2};  //seed.GetPin(PIN_GATE_IN_1);
+    gate_input[GATE_IN_1].Init(&pin);
+    pin = {DSY_GPIOB, 10}; //seed.GetPin(PIN_GATE_IN_2);
+    gate_input[GATE_IN_2].Init(&pin);
+    pin = {DSY_GPIOC, 13}; //seed.GetPin(PIN_GATE_IN_3);
+    gate_input[GATE_IN_3].Init(&pin);
+    pin = {DSY_GPIOC, 14}; //seed.GetPin(PIN_GATE_IN_4);
+    gate_input[GATE_IN_4].Init(&pin);    
+}
---
+++ src/daisy_heart-of-gold.h
@@ -0,0 +1,201 @@
+#pragma once
+#ifndef DSY_HEARTOFGOLD_BSP_H
+#define DSY_HEARTOFGOLD_H
+#include "daisy_seed.h"
+
+namespace daisy
+{
+/**
+    @brief Class that handles initializing all of the hardware specific to the Heart of Gold, based on the Daisy Patch Board. \n 
+    Helper funtions are also in place to provide easy access to built-in controls and peripherals.
+    @author Stephen Hensley, Making Sound Machines
+    @date March 2020
+    @ingroup boards
+*/
+class DaisyHeartOfGold
+{
+  public:
+    /** Enum of Ctrls to represent the four CV/Knob combos on the Patch
+     */
+    enum Ctrl
+    {
+        CTRL_1,    /**< */
+        CTRL_2,    /**< */
+        CTRL_3,    /**< */
+        CTRL_4,    /**< */
+        CTRL_5,    /**< */
+        CTRL_6,    /**< */
+        CTRL_7,    /**< */
+        CTRL_8,    /**< */
+        CTRL_9,    /**< */
+        CTRL_10,    /**< */
+        CTRL_11,    /**< */
+        CTRL_12,    /**< */
+        CTRL_LAST, /**< */
+    };
+
+    /** Daisy patch gate inputs */
+    enum GateInput
+    {
+        GATE_IN_1,    /**< */
+        GATE_IN_2,    /** <*/
+        GATE_IN_3,    /** <*/
+        GATE_IN_4,    /** <*/
+        GATE_IN_LAST, /**< */
+    };
+
+    /** Daisy patch gate inputs */
+    enum GateOutput
+    {
+        GATE_OUT_1,    /**< */
+        GATE_OUT_2,    /** <*/
+        GATE_OUT_3,    /** <*/
+        GATE_OUT_4,    /** <*/
+        GATE_OUT_LAST, /**< */
+    }; 
+    
+    /** Daisy patch gate inputs */
+    enum RouteButton
+    {
+        ROUTE_BUTTON_1,    /**< */
+        ROUTE_BUTTON_2,    /** <*/
+        ROUTE_BUTTON_3,    /** <*/
+        ROUTE_BUTTON_4,    /** <*/
+        ROUTE_BUTTON_LAST, /**< */
+    };    
+
+    /** Daisy patch gate inputs */
+    enum Enc
+    {
+        ENC_1,    /**< */
+        ENC_2,    /** <*/
+        ENC_3,    /** <*/
+        ENC_4,    /** <*/
+        ENC_LAST, /**< */
+    };     
+
+    /** Shift Register Pins */
+    enum SR595Pin
+    {
+        SR_LATCH,    /**< */
+        SR_CLOCK,    /** <*/
+        SR_DATA,    /** <*/
+        SR_LAST, /**< */
+    };
+
+
+    /** Constructor */
+    DaisyHeartOfGold() {}
+    /** Destructor */
+    ~DaisyHeartOfGold() {}
+
+    /** Initializes the daisy seed, and patch hardware.*/
+    void Init(bool boost = false);
+
+    /** 
+    Wait some ms before going on.
+    \param del Delay time in ms.
+    */
+    void DelayMs(size_t del);
+
+
+    /** Starts the callback
+    \cb multichannel callback function
+    */
+    void StartAudio(AudioHandle::AudioCallback cb);
+
+    /**
+       Switch callback functions
+       \param cb New multichannel callback function.
+    */
+    void ChangeAudioCallback(AudioHandle::AudioCallback cb);
+
+    /** Stops the audio */
+    void StopAudio();
+
+    /** Set the sample rate for the audio */
+    void SetAudioSampleRate(SaiHandle::Config::SampleRate samplerate);
+
+    /** Get sample rate */
+    float AudioSampleRate();
+
+    /** Audio Block size defaults to 48.
+    Change it using this function before StartingAudio
+    \param size Audio block size.
+    */
+    void SetAudioBlockSize(size_t size);
+
+    /** Returns the number of samples per channel in a block of audio. */
+    size_t AudioBlockSize();
+
+    /** Returns the rate in Hz that the Audio callback is called */
+    float AudioCallbackRate();
+
+    /** Start analog to digital conversion.*/
+    void StartAdc();
+
+    /** Stops Transfering data from the ADC */
+    void StopAdc();
+
+
+    /** Call at same rate as reading controls for good reads. */
+    void ProcessAnalogControls();
+
+    /** Process Analog and Digital Controls */
+    inline void ProcessAllControls()
+    {
+        ProcessAnalogControls();
+        ProcessDigitalControls();
+    }
+
+    /**
+       Get value for a particular control
+       \param k Which control to get
+     */
+    float GetKnobValue(Ctrl k);
+
+    /**  Process the digital controls */
+    void ProcessDigitalControls();
+
+    /**  Control the display */
+    void DisplayControls(bool invert = true);
+
+    /* These are exposed for the user to access and manipulate directly
+       Helper functions above provide easier access to much of what they are capable of.
+    */
+    DaisySeed     seed;                             /**< Seed object */
+    Encoder       encoder[ENC_LAST];                /**< Encoder object */
+    AnalogControl controls[CTRL_LAST];              /**< Array of controls*/
+    GateIn        gate_input[GATE_IN_LAST];         /**< Gate inputs  */
+    GateIn        route_button[ROUTE_BUTTON_LAST];  /**< Route Buttons  */
+    MidiHandler   midi;                             /**< Handles midi*/
+    Dac8568       dac_8568;                         /**< Handles ext DAC*/
+    /** OledDisplay   display; */                   /**< & */
+
+    // TODO: Add class for Gate output
+    dsy_gpio gate_output[GATE_OUT_LAST]; /**< Gate outputs  */
+
+    dsy_gpio_pin sr_pin_cfg[SR_LAST];
+
+    ShiftRegister595 sr_595;
+
+  private:
+    void InitAudio();
+    void InitControls();
+    /** void InitDisplay(); */
+    void InitMidi();
+    void InitCvOutputs();
+    void InitEncoder();
+    void InitGates();
+    void InitRouteButtons();
+    void InitSR595();
+    void InitDAC8568();
+
+    dsy_gpio ak4556_reset_pin_;
+    uint32_t screen_update_last_, screen_update_period_;
+    size_t num_sr_chained;
+};
+
+} // namespace daisy
+
+#endif
