427357fa2339f3c664bbe684b442d2664ae2c4bf
makingsoundmachines <roland@groovelastig.de>
2021-04-02 00:06:28 +0200

    LED Matrix ported functions from Adafruit library

--- src/dev/led_is31fl3731.cpp
+++ src/dev/led_is31fl3731.cpp
@@ -4,8 +4,10 @@
 #include "../per/gpio.h"
 #include "../sys/system.h"
 
+
 // Driver for ISSI IS31FL3731 144 ch PWM LED Matrix
-// Port for Daisy by Making Sound Machines  - https://github.com/makingsoundmachines
+// Ported from the Adafruit IS31FL3731 library - https://github.com/adafruit/Adafruit_IS31FL3731/
+// Port for Daisy by Making Sound Machines     - https://github.com/makingsoundmachines
 
 // Pin Names in Schematics
 //
@@ -14,6 +16,15 @@
 // PIN_ENC_B   I2C1_SDA  11   PB8       SDA
 // PIN_ENC_A   I2C1_SCL  12   PB9       SCL
 
+
+#ifndef _swap_int16_t
+#define _swap_int16_t(a, b)                                                    \
+  {                                                                            \
+    int16_t t = a;                                                             \
+    a = b;                                                                     \
+    b = t;                                                                     \
+  }
+#endif
 
 // Commands
 
@@ -102,25 +113,15 @@ void Is31fl3731::Init(const I2CHandle i2c, const uint8_t * addresses, uint8_t nu
     Is31fl3731_.Initialized = 1;
 }
 
-/*
-void Is31fl3731::Write(int channel) {
-    WriteIs31fl3731(WRITE_INPUT_REGISTER_UPDATE_N, channel, _values[channel], 15);
-}
 
-void Is31fl3731::Write() {
-    for (int channel = 0; channel < Channels; ++channel) {
-        WriteIs31fl3731(channel == 7 ? WRITE_INPUT_REGISTER_UPDATE_ALL : WRITE_INPUT_REGISTER, channel, _values[channel], 0);
-    }
-}
-*/
 
-void Is31fl3731::WriteIs31fl3731(uint8_t address, uint8_t command, uint8_t data) {
+void Is31fl3731::WriteIs31fl3731(uint8_t i2c_address, uint8_t reg, uint8_t data) {
     uint8_t b[2];
 
-    b[0] = command;
+    b[0] = reg;
     b[1] = data;
 
-    i2c_.TransmitBlocking(address, b, 2, 1000);
+    i2c_.TransmitBlocking(i2c_address, b, 2, 1); // 1000
 }
 
 
@@ -150,19 +151,155 @@ void Is31fl3731::Test_mode()
 }
 
 
-/*
-void Is31fl3731::Reset() {
-    WriteIs31fl3731(RESET_POWER_ON, 0, 0, 0);
-    // hal::Delay::delay_us(50);
+
+
+
+void Is31fl3731::Begin(const I2CHandle i2c, const uint8_t * addresses, uint8_t numDrivers) {
+
+    i2c_ = i2c;
+    numDrivers_ = numDrivers;
+    for(int d = 0; d < numDrivers_; d++)
+        addresses_[d] = addresses[d];
+
+    _width  = WIDTH  = 16;
+    _height = HEIGHT =  9;
+    rotation = 0;
+
+    // init the individual drivers
+    for(int d = 0; d < numDrivers_; d++)
+    {
+        // shutdown
+        WriteRegister8(addresses_[d], ISSI_BANK_FUNCTIONREG, ISSI_REG_SHUTDOWN, 0x00);
+        //delay(10);
+
+        // out of shutdown
+        WriteRegister8(addresses_[d], ISSI_BANK_FUNCTIONREG, ISSI_REG_SHUTDOWN, 0x01);
+
+        // picture mode
+        WriteRegister8(addresses_[d], ISSI_BANK_FUNCTIONREG, ISSI_REG_CONFIG, ISSI_REG_CONFIG_PICTUREMODE);
+
+        DisplayFrame(addresses_[d], _frame);
+
+        // all LEDs on & 0 PWM
+        Clear(addresses_[d]); // set each led to 0 PWM
+
+        for (uint8_t f = 0; f < 8; f++) {
+        for (uint8_t i = 0; i <= 0x11; i++)
+            WriteRegister8(addresses_[d], f, i, 0xff); // each 8 LEDs on
+        }
+        AudioSync(addresses_[d], false);
+    }
+    Is31fl3731_.Initialized = 1;
 }
 
-void Is31fl3731::SetInternalRef(bool enabled) {
-    WriteIs31fl3731(SETUP_INTERNAL_REF, 0, 0, enabled ? 1 : 0);
+
+
+void Is31fl3731::Clear(uint8_t i2c_address) {
+    uint8_t b[2];
+
+    //select Bank (current frame)
+    b[0] = ISSI_COMMANDREGISTER;
+    b[1] = _frame;
+    i2c_.TransmitBlocking(i2c_address, b, 2, 1);
+
+    uint8_t msg[25];
+
+    for (uint8_t i = 0; i < 6; i++) {
+        
+        msg[0] = 0x24 + i * 24;
+
+        // write 24 bytes to 0 at once
+        for (uint8_t j = 1; j < 25; j++) {
+            msg[j] = 0;
+        }
+
+        i2c_.TransmitBlocking(i2c_address, msg, 25, 1);
+    }
 }
 
-void Is31fl3731::SetClearCode(ClearCode code) {
-    WriteIs31fl3731(LOAD_CLEAR_CODE_REGISTER, 0, 0, code);
+void Is31fl3731::SetLEDPWM(uint8_t i2c_address, uint8_t lednum, uint8_t pwm, uint8_t bank) {
+    if (lednum >= 144)
+        return;
+    WriteRegister8(i2c_address, bank, (0x24 + lednum), pwm);
 }
-*/
 
 
+void Is31fl3731::DrawPixel(uint8_t i2c_address, int16_t x, int16_t y, uint8_t pwm) {
+    // check rotation, move pixel around if necessary
+    switch (getRotation()) {
+        case 1:
+            _swap_int16_t(x, y);
+            x = 16 - x - 1;
+        break;
+        case 2:
+            x = 16 - x - 1;
+            y = 9 - y - 1;
+        break;
+        case 3:
+            _swap_int16_t(x, y);
+            y = 9 - y - 1;
+        break;
+    }
+
+    if ((x < 0) || (x >= 16))
+        return;
+    if ((y < 0) || (y >= 9))
+        return;
+    if (pwm > 255)
+    pwm = 255; // PWM 8bit max
+
+    SetLEDPWM(i2c_address, x + y * 16, pwm, _frame);
+    return;
+}
+
+
+
+// Make this work for multiple chips
+void Is31fl3731::SetFrame(uint8_t frame) { _frame = frame; }
+
+
+void Is31fl3731::DisplayFrame(uint8_t i2c_address, uint8_t frame) {
+    if (frame > 7) 
+        frame = 0; 
+    WriteRegister8(i2c_address, ISSI_BANK_FUNCTIONREG, ISSI_REG_PICTUREFRAME, frame);
+}
+
+
+void Is31fl3731::AudioSync(uint8_t i2c_address, bool sync) {
+    if (sync) {
+        WriteRegister8(i2c_address, ISSI_BANK_FUNCTIONREG, ISSI_REG_AUDIOSYNC, 0x1);
+    } else {
+        WriteRegister8(i2c_address, ISSI_BANK_FUNCTIONREG, ISSI_REG_AUDIOSYNC, 0x0);
+    }
+}
+
+
+void Is31fl3731::WriteRegister8(uint8_t i2c_address, uint8_t bank, uint8_t reg, uint8_t data) {
+    uint8_t b[2];
+
+    //select Bank
+    b[0] = ISSI_COMMANDREGISTER;
+    b[1] = bank;
+    i2c_.TransmitBlocking(i2c_address, b, 2, 1);
+
+    // send data
+    b[0] = reg;
+    b[1] = data;
+    i2c_.TransmitBlocking(i2c_address, b, 2, 1);
+}
+
+void Is31fl3731::setRotation(uint8_t x) {
+  rotation = (x & 3);
+  switch (rotation) {
+  case 0:
+  case 2:
+    _width = WIDTH;
+    _height = HEIGHT;
+    break;
+  case 1:
+  case 3:
+    _width = HEIGHT;
+    _height = WIDTH;
+    break;
+  }
+}
--- src/dev/led_is31fl3731.h
+++ src/dev/led_is31fl3731.h
@@ -15,7 +15,8 @@ namespace daisy
 
 /** 
  * Driver for ISSI IS31FL3731 144 ch PWM LED Matrix
- * Port for Daisy by Making Sound Machines  - https://github.com/makingsoundmachines
+ * Ported from the Adafruit IS31FL3731 library - https://github.com/adafruit/Adafruit_IS31FL3731/
+ * Port for Daisy by Making Sound Machines     - https://github.com/makingsoundmachines
  *
  * LED driver for one or multiple ISSI IS31FL3731 144ch 8bit PWM chips connected to
  * a single I2C peripheral.
@@ -30,37 +31,127 @@ class Is31fl3731
     Is31fl3731() {}
     ~Is31fl3731() {}
 
-
-    // configuration currently only uses SPI1, w/ soft chip select.
-
     /** 
-    Initialize ISSI IS31FL3731 driver
-
+     * @brief Initialize ISSI IS31FL3731 driver
      * \param i2c           The I2C peripheral to use.
      * \param addresses     An array of addresses for each of the driver chips.
      * \param numDrivers    The number of IS31FL3731 drivers attached to the I2C
      *                      peripheral.
     */    
-
     void Init(const I2CHandle i2c, const uint8_t * addresses, uint8_t numDrivers);
 
+    /** 
+     * @brief Alternate Init (Adafruit) - Initialize hardware and clear display
+     * \param i2c           The I2C peripheral to use.
+     * \param addresses     An array of addresses for each of the driver chips.
+     * \param numDrivers    The number of IS31FL3731 drivers attached to the I2C
+     *                      peripheral.
+    */
+    void Begin(const I2CHandle i2c, const uint8_t * addresses, uint8_t numDrivers);
+
+    /** 
+     *  @brief Test mode from the ISSI code example - LFO-pulses all LEDs
+    */
     void Test_mode();
 
-    /*void Set(int channel, Value value) {
-        _values[channel] = value;
-    }
+    /** 
+     *  @brief Sets all LEDs on & 0 PWM for current frame.
+     *  @param i2c_adress The I2C adress
+    */
+    void Clear(uint8_t i2c_address);
 
-    void Write(int channel);
-    void Write();*/
+    /** 
+     *  @brief Low level accesssor - sets a 8-bit PWM pixel value to a bank location
+     *  does not handle rotation, x/y or any rearrangements!
+     *  @param i2c_adress The I2C adress
+     *  @param lednum The offset into the bank that corresponds to the LED
+     *  @param bank The bank/frame we will set the data in
+     *  @param pwm brightnes, from 0 (off) to 255 (max on)
+    */
+    void SetLEDPWM(uint8_t i2c_address, uint8_t lednum, uint8_t pwm, uint8_t bank);    
+
+    /** 
+     *  @brief Sets a 8-bit PWM pixel value, handles rotation 
+     *  and pixel arrangement, unlike setLEDPWM
+     *  @param i2c_adress The I2C adress
+     *  @param x The x position, starting with 0 for left-most side
+     *  @param y The y position, starting with 0 for top-most side
+     *  @param pwm takes 0 (off) to 255 (max on)
+    */
+    void DrawPixel(uint8_t i2c_address, int16_t x, int16_t y, uint8_t pwm);    
+
+    /** 
+     *  @brief Set's this object's frame tracker (does not talk to the chip)
+     *  @param frame Ranges from 0 - 7 for the 8 frames
+     *
+     *  TODO: Make this work for multiple chips
+    */
+    void SetFrame(uint8_t frame);
+
+    /** 
+     *  @brief Have the chip set the display to the contents of a frame
+     *  @param i2c_adress The I2C adress
+     *  @param frame Ranges from 0 - 7 for the 8 frames
+    */
+    void DisplayFrame(uint8_t i2c_address, uint8_t frame);    
+
+    /** 
+     *  @brief Enable the audio 'sync' for brightness pulsing (untested)
+     *  @param i2c_adress The I2C adress
+     *  @param sync True to enable, False to disable
+    */
+    void AudioSync(uint8_t i2c_address, bool sync);
+
+    /** 
+     *  @brief Write one byte to a register located in a given bank
+     *  @param i2c_adress The I2C adress
+     *  @param bank The IS31 bank to write the register location
+     *  @param reg the offset into the bank to write
+     *  @param data The byte value
+    */
+    void WriteRegister8(uint8_t i2c_address, uint8_t bank, uint8_t reg, uint8_t data);
+
+    /** 
+     *  @brief Write one byte to a register
+     *  @param i2c_adress The I2C adress
+     *  @param reg the offset into the bank to write
+     *  @param data The byte value
+    */
+    void WriteIs31fl3731(uint8_t address, uint8_t reg, uint8_t data);
+
+
+
+    /************************************************************************/
+    /*!
+      @brief      Get rotation setting for display
+      @returns    0 thru 3 corresponding to 4 cardinal rotations
+    */
+    /************************************************************************/
+    uint8_t getRotation(void) const { return rotation; }
+
+    /************************************************************************/
+    /*!
+      @brief      Set rotation setting for display
+      @returns    0 thru 3 corresponding to 4 cardinal rotations
+    */
+    /************************************************************************/
+    void setRotation(uint8_t r);
 
   private:
-    void WriteIs31fl3731(uint8_t address, uint8_t command, uint8_t data);
-
     void Reset();
     void SetInternalRef(bool enabled);
 
     uint8_t * addresses_;
     uint8_t numDrivers_;
+
+    uint8_t _frame;
+
+    int16_t WIDTH = 16;    ///< This is the 'raw' display width - never changes
+    int16_t HEIGHT = 9;    ///< This is the 'raw' display height - never changes
+    int16_t _width;        ///< Display width as modified by current rotation
+    int16_t _height;       ///< Display height as modified by current rotation
+    uint8_t rotation = 0;  ///< Display rotation (0 thru 3)
+
       
 };
 /** @} */
